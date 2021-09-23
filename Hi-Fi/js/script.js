f43fc21adb5c256c957d14e143c04682b581cf2c
Ona <onushkis@gmail.com>
2021-09-23 11:07:27 +0200

    add new grid for shop

--- Hi-Fi/js/script.js
+++ Hi-Fi/js/script.js
@@ -1,22 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
   // ----------------her finde jeg Nav list
 
-  let categoriesContainer = document.querySelector(".categoriesContainer");
+  let categoriesContainer = document.querySelector(".Main");
 
   fetch("./js/shop.json")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      // console.log(data);
 
       // ----------------her finde alle Arrays og skriver i innerHtml det hele
 
       data.Categories.forEach((category) => {
-        console.log(category.image);
+        // console.log(category.image);
         categoriesContainer.innerHTML += `
-                <article class="" style="width: 255px; height: 255px; background-image: url('./Imagesshop/${category.image}')">
-                    <a href="#" class="categoriesContainer__a ">${category.categoryName}</a>
-                </article>
+        
+        <div class="Main__Nav__gallery">
+
+        <div class="Main__imgShop">
+
+<img src="images/category/${category.image}" alt="">
+
+        </div>
+
+        <div class="Main__img__name">
+<p>${category.Main__img__name}</p>
+
+        </div>
+
+    </div>
+
+    
+    
     `;
+
+   
       });
     });
 });
